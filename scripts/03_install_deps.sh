c8f99a6828070cae580f033d3c72ac3b53c9384b
Stefan Ionescu <forgetthismail@protonmail.com>
2025-09-08 21:40:06 +0100

    Fix

--- scripts/03_install_deps.sh
+++ scripts/03_install_deps.sh
@@ -5,7 +5,15 @@ ROOT_DIR="$(cd "${SCRIPT_DIR}/.." && pwd)"
 source "${SCRIPT_DIR}/utils.sh"
 
 log_info "Installing Python dependencies"
-python -m pip install --upgrade pip
-python -m pip install -r "${ROOT_DIR}/requirements.txt"
+
+export PIP_ROOT_USER_ACTION=${PIP_ROOT_USER_ACTION:-ignore}
+
+if [ ! -d "${ROOT_DIR}/.venv" ]; then
+  log_info "Creating virtual environment at ${ROOT_DIR}/.venv"
+  python -m venv "${ROOT_DIR}/.venv"
+fi
+
+"${ROOT_DIR}/.venv/bin/python" -m pip install --upgrade pip
+"${ROOT_DIR}/.venv/bin/pip" install -r "${ROOT_DIR}/requirements.txt"
 
 
