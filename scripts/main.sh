7b61e5ad273b7051f0c3d9f6f46908f4dfb3572a
Stefan Ionescu <forgetthismail@protonmail.com>
2025-09-09 14:36:15 +0100

    Bells and whistles

--- scripts/main.sh
+++ scripts/main.sh
@@ -14,5 +14,6 @@ bash "${SCRIPT_DIR}/03_install_deps.sh"
 bash "${SCRIPT_DIR}/04_prepare_lmcache.sh"
 bash "${SCRIPT_DIR}/05_env_defaults.sh"
 bash "${SCRIPT_DIR}/06_start_server.sh"
+bash "${SCRIPT_DIR}/07_follow_logs.sh"
 
 
