8bf222737379e4987070110f53587e9ee02b2f47
Tiles <stefan@thetilesgame.com>
2025-10-30 02:17:22 +0000

    Fully align docker with scripts

--- docker/awq/scripts/build/context.sh
+++ docker/awq/scripts/build/context.sh
@@ -8,7 +8,7 @@ prepare_build_context() {
   cp -a "${DOCKERFILE}" "${TMP_BUILD_DIR}/Dockerfile"
   cp -a "${SCRIPT_DIR}/scripts" "${TMP_BUILD_DIR}/scripts"
   cp -a "${ROOT_DIR}/requirements.txt" "${TMP_BUILD_DIR}/requirements.txt"
-  cp -a "${ROOT_DIR}/prompts.py" "${TMP_BUILD_DIR}/prompts.py"
+  cp -a "${ROOT_DIR}/prompts" "${TMP_BUILD_DIR}/prompts"
   cp -a "${ROOT_DIR}/src" "${TMP_BUILD_DIR}/src"
 
   BUILD_CONTEXT="${TMP_BUILD_DIR}"
