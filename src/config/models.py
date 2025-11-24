537d43a44b38e81f6402ef3e4a42142e8d3f738f
Tiles <stefan@thetilesgame.com>
2025-11-24 19:24:49 +0000

    Fix

--- src/config/models.py
+++ src/config/models.py
@@ -18,7 +18,7 @@ ALLOWED_CHAT_MODELS: list[str] = [
 
     "TheDrummer/Rocinante-12B-v1.1", # mid intelligence, good if you need to downgrade quality temporarily
     "knifeayumu/Cydonia-v1.3-Magnum-v4-22B", # good, need further tests
-    "Doctor-Shotgun/MS3.2-24B-Magnum-Diamond", # 3
+    "Doctor-Shotgun/MS3.2-24B-Magnum-Diamond",
     "anthracite-org/magnum-v2-32b", # a bit cheesy and yaps too much (good for general RP tho)
     "djuna/magnum-v2-32b-chatml", # yaps too much but might fix with prompting (good for general RP tho)
     "anthracite-org/magnum-v4-72b", # amazing on OpenRouter but too big on L40S even after 4bit quant
