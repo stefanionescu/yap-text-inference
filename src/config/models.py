3cf3dc7f54a694065a9163b22762001bdf1fe73c
Stefan Ionescu <forgetthismail@protonmail.com>
2025-12-09 15:14:43 +0000

    More models

--- src/config/models.py
+++ src/config/models.py
@@ -36,6 +36,8 @@ ALLOWED_CHAT_MODELS: list[str] = [
     "baidu/ERNIE-4.5-21B-A3B-PT",
     "moonshotai/Moonlight-16B-A3B-Instruct",
     "kakaocorp/kanana-1.5-15.7b-a3b-instruct",
+
+    "Qwen/Qwen2-57B-A14B",
     
     # Pre-quantized GPTQ models
     "SicariusSicariiStuff/Impish_Nemo_12B_GPTQ_4-bit-32",
@@ -52,6 +54,8 @@ ALLOWED_CHAT_MODELS: list[str] = [
     "cpatonn/Qwen3-Next-80B-A3B-Instruct-AWQ-4bit",
     "warshanks/Ministral-8B-Instruct-2410-AWQ",
     "jeffcookio/Mistral-Small-3.2-24B-Instruct-2506-awq-sym",
+
+    "TheBloke/mixtral-8x7b-v0.1-AWQ",
 
     "yapwithai/zerofata-MS3.2-paintedfantasy-visage-33B-w4a16", # really bad, rambles even with conservative params
     "yapwithai/sicariussicariistuff-impish-nemo-12B-w4a16",
@@ -92,6 +96,8 @@ ALLOWED_DUAL_MODELS: list[str] = [
     "cpatonn/Qwen3-30B-A3B-Instruct-2507-AWQ-4bit",
     "cyankiwi/Kimi-Linear-48B-A3B-Instruct-AWQ-4bit",
     "cpatonn/Qwen3-Next-80B-A3B-Instruct-AWQ-4bit",
+    "TheBloke/mixtral-8x7b-v0.1-AWQ",
+    "Qwen/Qwen2-57B-A14B",
 ]
 
 
