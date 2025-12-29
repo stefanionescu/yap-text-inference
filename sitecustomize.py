9ed8789b092e9d7c097874c4e94050aff51405ae
Stefan Ionescu <forgetthismail@protonmail.com>
2025-12-29 22:46:34 +0000

    Small fixes

--- sitecustomize.py
+++
@@ -1,7 +0,0 @@
-"""Ensure project-wide site customizations run for every Python invocation."""
-
-from __future__ import annotations
-
-from src.scripts import site_customize as _site_customize  # noqa: F401
-
-__all__ = getattr(_site_customize, "__all__", [])
